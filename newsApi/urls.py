68d09332f0eddab6438a84b4770f6cb769018eba
ahsansadman <sadmannafim123@gmail.com>
2022-06-23 02:07:31 +0600

    phase 1 completed(fetching news)

--- newsApi/urls.py
+++ newsApi/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from newsapp import views
-from knox import views as knox_views
+from django.urls import path
 
+from newsapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', views.news)
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,1 +1,27 @@
 from django.shortcuts import render
+
+from newsapi import NewsApiClient
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# Create your views here.
+
+newsapi = NewsApiClient(api_key='f814e1c6740942ca9f5a6dfbfcf31d1b')
+
+
+@api_view()
+def news(request):
+    country = request.GET.get('country')
+    source = request.GET.get('source')
+
+    if country and source:
+       return Response("Select either country or source")
+
+    if country:
+        top_headlines = newsapi.get_top_headlines(
+                                            country=country)
+
+    if source:
+        top_headlines = newsapi.get_top_headlines(
+                                            sources=source)
+
+    return Response(top_headlines)
