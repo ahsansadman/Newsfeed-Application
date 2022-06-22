f706d276a97376ddb7d8e11d75bfa4b8778a36a5
ahsansadman <sadmannafim123@gmail.com>
2022-06-23 02:04:08 +0600

    models added

--- newsapp/models.py
+++ newsapp/models.py
@@ -4,3 +4,32 @@ from django_countries.fields import CountryField
 from taggit.managers import TaggableManager
 # Create your models here.
     
+from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+from taggit.managers import TaggableManager
+# Create your models here.
+    
+class Source(models.Model):
+    id = models.CharField(primary_key=True,max_length=50)
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.id
+    
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    country = CountryField(multiple=True)
+    tag = TaggableManager()
+    source = models.ManyToManyField(Source)
+    def __str__(self):
+        return self.user.username
+    
+class Newsfeed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    newsfeed = models.TextField()
+    
+    def __str__(self):
+        return self.user.username
