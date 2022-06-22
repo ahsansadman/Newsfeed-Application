3fc44926e9e6071e1b70f326c151e6293630e70d
ahsansadman <sadmannafim123@gmail.com>
2022-06-23 02:02:20 +0600

    init

---
+++ newsapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+from taggit.managers import TaggableManager
+# Create your models here.
+    
