e241cdd19ea79df8c875408e21a492616dbddea8
ahsansadman <sadmannafim123@gmail.com>
2022-06-23 02:35:58 +0600

    phase 4 completed

--- newsapp/admin.py
+++ newsapp/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import action
 from newsapp.models import *
-
 # Register your models here.
-
 class SourceAdmin(admin.TabularInline):
     model = Source
-
 class AccountAdmin(admin.ModelAdmin):
    inlines = [SourceAdmin,]
 
 admin.site.register(Account)
-admin.site.register(Source) 
+admin.site.register(Source)
+admin.site.register(Newsfeed) 
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,38 +1,34 @@
 from django.shortcuts import render
 from newsapi import NewsApiClient
+from requests.models import StreamConsumedError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics, status
 from .models import Account, Source
 from .serializers import *
-from rest_framework.authentication import BasicAuthentication
+from knox.auth import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-
 from django.contrib.auth import authenticate, login, logout
+import json
+from celery import shared_task
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+
+import sendgrid
+from sendgrid import SendGridAPIClient
+import os
+from sendgrid.helpers.mail import *
+
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+
 # Create your views here.
 
 newsapi = NewsApiClient(api_key='f814e1c6740942ca9f5a6dfbfcf31d1b')
 
-# @csrf_exempt
-@api_view(['GET', 'POST'])
-def login(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return Response("User is logged in")
-        ...
-    else:
-        return Response("login failed")
-
-
-@api_view()
-def logout_view(request):
-    logout(request)
-    return Response("User is logged out")
-
-
+    
 @api_view()
 def news(request):
     country = request.GET.get('country')
@@ -50,6 +46,7 @@ def news(request):
                                             sources=source)
         
     return Response(top_headlines)
+
 @api_view()
 def update_sources(request):
     sources = newsapi.get_sources()['sources']
@@ -59,19 +56,111 @@ def update_sources(request):
         source_data.save()
     
     return Response(sources)
+
+@shared_task
+def get_newsfeed(account):
+    
+    countries = account.country
+    
+    source_list = '' 
+    source_set = account.source.all()     
+    for source in source_set:
+        if not source_list:
+            source_list = source_list + source.id 
+        else:
+            source_list = source_list +  ',' +source.id 
+            
+    newsfeed = []
+    keyword = list(account.tag.names())
+    tag_apear = False
+    # print(keyword)
+    for country in countries:
+        # print(country.code)
+        news_list =  newsapi.get_top_headlines(
+                                country=str(country.code).lower())
+                
+        for news in news_list['articles']:
+            if news['source']['id'] is None:
+                continue
+            elif news['source']['id'] in source_list:
+                item = {
+                    'headline' : news['title'],
+                    'thumbnail': news['urlToImage'],
+                    'source' : news['url'],
+                    'country' : country.name,
+                }
+                newsfeed.append(item)
+                for key in keyword:
+                    if key in news['title']:
+                        sendemail(account,news['title'],key)          
+   
+    try:
+        feed_info = Newsfeed.objects.get(user=account.user)
+        feed_info.newsfeed = json.dumps(newsfeed)
+        feed_info.save()
+    except Newsfeed.DoesNotExist:
+        feed_info = Newsfeed.objects.create(user = account.user,newsfeed=json.dumps(newsfeed))
+        feed_info.save() 
+    return newsfeed
+
+
+def sendemail(account,content,keyword):
+    
+    message = Mail(
+        from_email='ahsan.sadman@ferntechsolutions.com',
+        to_emails=account.user.email,
+        subject= str(keyword) +' keyword appeared in your personalized news feed',
+        html_content='<strong>'+ content +'</strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
+        
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })    
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
+
 class UserAccountDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-    authentication_classes = [BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
   
     def get(self,request):
         try:
             account = Account.objects.get(user=request.user)
             serializer = AccountSerializer(account)
+            # sendemail(account,'hello', 'say')
             return Response(serializer.data)           
         except Account.DoesNotExist:
-            return Response('No account found')    
+            return Response('No account found')
+            
+
     def patch(self, request):
         try:
             
@@ -83,6 +172,7 @@ class UserAccountDetail(generics.RetrieveUpdateDestroyAPIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Account.DoesNotExist:
             raise Response('Update failed')
+
     def delete(self, request):
         try:
             
@@ -92,47 +182,21 @@ class UserAccountDetail(generics.RetrieveUpdateDestroyAPIView):
             return Response(status=status.HTTP_204_NO_CONTENT) 
         except Account.DoesNotExist:
             raise 'Deletion failed'          
-
+        
 
 class UserNewsfeed(generics.RetrieveUpdateDestroyAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-    authentication_classes = [BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
-
+    
     def get(self,request):
         try:
+            newsfeed_data = Newsfeed.objects.get(user=request.user)
+            return Response(json.loads(newsfeed_data.newsfeed)) 
+                  
+        except Newsfeed.DoesNotExist:
             account = Account.objects.get(user=request.user)
-            countries = account.country
+            newsfeed = get_newsfeed(account)
+            return Response(newsfeed)         
 
-            source_list = '' 
-            source_set = account.source.all()     
-            for source in source_set:
-                if not source_list:
-                    source_list = source_list + source.id 
-                else:
-                    source_list = source_list +  ',' +source.id 
-
-            newsfeed = []
-
-            for country in countries:
-                # print(country.code)
-                news_list =  newsapi.get_top_headlines(
-                                        country=str(country.code).lower())
-                # print(news_list)           
-                for news in news_list['articles']:
-                    if news['source']['id'] is None:
-                        continue
-                    else:
-                        item = {
-                            'headline' : news['title'],
-                            'thumbnail': news['urlToImage'],
-                            'source' : news['url'],
-                            'country' : country.name,
-                        }
-                        newsfeed.append(item) 
-
-
-            return Response(newsfeed)           
-        except Account.DoesNotExist:
-            return Response('No account found')    
