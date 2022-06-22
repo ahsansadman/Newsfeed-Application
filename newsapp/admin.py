019072f92ad425da29176c2b0ee79c7d8168c341
ahsansadman <sadmannafim123@gmail.com>
2022-06-23 02:04:51 +0600

    admin modified

--- newsapp/admin.py
+++ newsapp/admin.py
@@ -4,3 +4,12 @@ from newsapp.models import *
 
 # Register your models here.
 
+class SourceAdmin(admin.TabularInline):
+    model = Source
+
+class AccountAdmin(admin.ModelAdmin):
+   inlines = [SourceAdmin,]
+
+admin.site.register(Account)
+admin.site.register(Source)
+admin.site.register(Newsfeed)
